772002dc6e0330eb21316e1c19c009fec95e2f10
Murisi <moham714854@gmail.com>
2025-09-17 22:54:38 +0800

    first deployment ims fix categeries

--- app/inventory/categories/Table.tsx
+++ app/inventory/categories/Table.tsx
@@ -1,21 +1,14 @@
 import { DataTable } from "@/components/common/Table";
 import React from "react";
-import { Clock, Loader2, CheckCircle, XCircle } from "lucide-react";
 
 import { columns } from "./columns";
-import CardContainer from "@/components/common/CardContainer";
 
 import { fetchCategory } from "@/app/actions/roles";
-import Addnewcategorie from "./addnewcategorie";
 
 export default async function Table() {
   const data = await fetchCategory();
   return (
-    <CardContainer
-      title="Users"
-      total={data.length}
-      action={<Addnewcategorie />}
-    >
+    <>
       {data.length > 0 ? (
         <DataTable
           data={data}
@@ -27,6 +20,6 @@ export default async function Table() {
       ) : (
         <div>no users</div>
       )}
-    </CardContainer>
+    </>
   );
 }
