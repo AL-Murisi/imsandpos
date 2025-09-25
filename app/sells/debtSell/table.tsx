bce501882f0d763cb042e1be0a543b26fb301f48
Murisi <moham714854@gmail.com>
2025-09-25 17:32:02 +0300

    adding some optiminztion to search1

--- app/sells/debtSell/table.tsx
+++ app/sells/debtSell/table.tsx
@@ -67,7 +67,7 @@ export default function DebtSells({
           onGlobalFilterChange={setGlobalFilter}
           globalFilter={globalFilter}
           sorting={sort}
-          highet={"h-[70vh]"}
+          highet="h-[70vh]"
           pagination={pagination}
           totalCount={data.length}
         />
