38e28ff11c7fe9779ecef03c8c002fa8cb92e739
Murisi <moham714854@gmail.com>
2025-11-05 19:50:35 +0300

    chnage pwa

--- next.config.ts
+++ next.config.ts
@@ -25,42 +25,17 @@ const withNextIntl = createNextIntlPlugin();
 // });
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // Disable PWA in dev
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  fallbacks: {
+    document: "/offline",
+  },
   workboxOptions: {
     disableDevLogs: true,
   },
-  // Example runtime caching for an API endpoint
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/api\.example\.com\/data.*/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "example-api-cache",
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 60 * 60 * 24 * 7, // Cache for 1 week
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-    {
-      urlPattern: /.*/i,
-      handler: "NetworkOnly",
-      options: {
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-  ],
 });
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
