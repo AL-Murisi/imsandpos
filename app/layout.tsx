221fee6a80a20d3809e8eda4d5348060d1ade688
Murisi <moham714854@gmail.com>
2026-03-01 05:03:32 +0300

    adding icons and fix scanning

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,180 +11,10 @@ import { currencyConfig } from "@/currency/config";
 import { cookies } from "next/headers";
 import { CurrencyProvider } from "@/components/CurrencyProvider";
 
-const iosSplashScreens = [
-  {
-    href: "/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_landscape.png",
-    media:
-      "screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/10.5__iPad_Air_landscape.png",
-    media:
-      "screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/8.3__iPad_Mini_portrait.png",
-    media:
-      "screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_Air_landscape.png",
-    media:
-      "screen and (device-width: 420px) and (device-height: 912px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_landscape.png",
-    media:
-      "screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/13__iPad_Pro_M4_landscape.png",
-    media:
-      "screen and (device-width: 1032px) and (device-height: 1376px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/iPhone_16__iPhone_15_Pro__iPhone_15__iPhone_14_Pro_portrait.png",
-    media:
-      "screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png",
-    media:
-      "screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png",
-    media:
-      "screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png",
-    media:
-      "screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/10.5__iPad_Air_portrait.png",
-    media:
-      "screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_11__iPhone_XR_landscape.png",
-    media:
-      "screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_landscape.png",
-    media:
-      "screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/iPhone_17_Pro__iPhone_17__iPhone_16_Pro_landscape.png",
-    media:
-      "screen and (device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/10.9__iPad_Air_landscape.png",
-    media:
-      "screen and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/12.9__iPad_Pro_landscape.png",
-    media:
-      "screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/iPhone_17_Pro__iPhone_17__iPhone_16_Pro_portrait.png",
-    media:
-      "screen and (device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/11__iPad_Pro_M4_portrait.png",
-    media:
-      "screen and (device-width: 834px) and (device-height: 1210px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_16_Plus__iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png",
-    media:
-      "screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png",
-    media:
-      "screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_landscape.png",
-    media:
-      "screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/10.2__iPad_portrait.png",
-    media:
-      "screen and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png",
-    media:
-      "screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/iPhone_17_Pro_Max__iPhone_16_Pro_Max_portrait.png",
-    media:
-      "screen and (device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_Air_portrait.png",
-    media:
-      "screen and (device-width: 420px) and (device-height: 912px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_16e__iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png",
-    media:
-      "screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_17_Pro_Max__iPhone_16_Pro_Max_landscape.png",
-    media:
-      "screen and (device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/11__iPad_Pro_M4_landscape.png",
-    media:
-      "screen and (device-width: 834px) and (device-height: 1210px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/11__iPad_Pro__10.5__iPad_Pro_landscape.png",
-    media:
-      "screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_landscape.png",
-    media:
-      "screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",
-  },
-  {
-    href: "/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png",
-    media:
-      "screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/12.9__iPad_Pro_portrait.png",
-    media:
-      "screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-  },
-  {
-    href: "/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png",
-    media:
-      "screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-  },
-];
-
 export const metadata: Metadata = {
-  title: "IMS - Inventory Management System",
-  description: "Complete Inventory Management and POS Solution",
-  generator: "Next.js",
-  manifest: "/manifest.json",
-  applicationName: "IMS",
+  title: "IMS - نظام إدارة المخزون | Inventory Management System",
+  description:
+    "نظام متكامل لإدارة المخزون ونقاط البيع (POS) والمستودعات - Complete Inventory Management, Warehouse, and POS Solution",
   keywords: [
     "inventory",
     "management",
@@ -193,7 +23,18 @@ export const metadata: Metadata = {
     "pwa",
     "nextjs",
     "warehouse",
+    "إدارة المخزون",
+    "نظام نقاط البيع",
+    "برنامج مستودعات",
+    "برنامج محاسبة",
+    "كاشير",
+    "مخازن",
+    "نظام كاشير",
+    "إدارة المبيعات",
   ],
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  applicationName: "IMS",
 
   appleWebApp: {
     capable: true,
@@ -206,9 +47,7 @@ export const metadata: Metadata = {
     address: true,
   },
 };
-
 type CurrencyKey = keyof typeof currencyConfig;
-
 export const viewport: Viewport = {
   themeColor: "#0b142a",
   minimumScale: 1,
@@ -234,22 +73,7 @@ export default async function RootLayout({
       : "YER";
 
   return (
-    <html lang={locale} suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://va.vercel-scripts.com" />
-        <link rel="dns-prefetch" href="https://va.vercel-scripts.com" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <link rel="icon" href="/icon.png" sizes="512x512" />
-        {iosSplashScreens.map((splash) => (
-          <link
-            key={splash.href}
-            rel="apple-touch-startup-image"
-            media={splash.media}
-            href={splash.href}
-          />
-        ))}
-      </head>
+    <html lang={locale} className={``} suppressHydrationWarning>
       <body className="font-sans">
         <ThemeProvider
           attribute="class"
@@ -257,6 +81,15 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <header>
+            <link rel="preconnect" href="https://va.vercel-scripts.com" />
+            <link rel="dns-prefetch" href="https://va.vercel-scripts.com" />
+
+            <link rel="manifest" href="/manifest.json" />
+
+            <link rel="icon" href="/favicon.ico" sizes="512x512" />
+          </header>
+
           <AuthProvider>
             <NextIntlClientProvider locale={locale} messages={messages[locale]}>
               <CurrencyProvider currency={currencyConfig[currencyKey]}>
